60664ccb978b34910ebea761fece426eb73338a1
Pasha <andreevpavel135@gmail.com>
2018-09-30 16:28:18 +0300

    FirstVersion

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String x = " ";
+        while (x != "exit") {
+            switch (x) {
+                case "add country":
+                    break;
+                case "edit country":
+                    break;
+                case "add city":
+                    break;
+                case "edit city":
+                    break;
+                case "help":
+                    break;
+                case "exit":
+                    x = "Exit";
+                    break;
+                default: System.out.println("Неизвестная команда");
+            }
+        }
+    }
+
+    public static void
+}
